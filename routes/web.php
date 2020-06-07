0b5c8c52f09dd7e30f753b7a1d982e3430648b13
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-06-07 12:51:25 +0900

    Eloquentでの一覧表示と検索機能実装

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Helloページ
 Route::get('hello', 'HelloController@index');
 Route::get('hello/add', 'HelloController@add');
 Route::post('hello/add', 'HelloController@create');
@@ -23,3 +24,8 @@ Route::post('hello/edit', 'HelloController@update');
 Route::get('hello/del', 'HelloController@del');
 Route::post('hello/del', 'HelloController@remove');
 Route::get('hello/show', 'HelloController@show');
+
+//Personページ
+Route::get('person', 'PersonController@index');
+Route::get('person/find', 'PersonController@find');
+Route::post('person/find', 'PersonController@search');
