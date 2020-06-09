138f0a13a8af805dc66cded5a9e183e0423e1871
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-06-09 21:53:45 +0900

    EloquentによるCRUD機能実装

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -18,10 +18,80 @@ class PersonController extends Controller
         return view('person.find', ['input' => '']);
     }
 
+    public function add(Request $request)
+    {
+        return view('person.add');
+    }
+
+    public function create(Request $request)
+    {
+        $this->validate($request, Person::$rules);
+        $person = new Person;
+        $form = $request->all();
+        unset($form['_token']);
+        $person->fill($form)->save();
+        return redirect('/person');
+    }
+
+    public function edit(Request $request)
+    {
+        $person = Person::find($request->id);
+        return view('person.edit', ['form' => $person]);
+    }
+
+    public function update(Request $request)
+    {
+        $this->validate($request, Person::$rules);
+        $person = Person::find($request->id);
+        $form = $request->all();
+        unset($form['_token']);
+        $person->fill($form)->save();
+        return redirect('/person');
+    }
+
+    public function delete(Request $request)
+    {
+        $person = Person::find($request->id);
+        return view('person.del', ['form' => $person]);
+    }
+
+    public function remove(Request $request)
+    {
+        Person::find($request->id)->delete();
+        return redirect('/person');
+    }
+
+    // public function search(Request $request)
+    // {
+    //     $item = Person::where('name', $request->input)->first();
+    //     $param = ['input' => $request->input, 'item' => $item];
+    //     return view('person.find', $param);
+    //     // $item = Person::find($request->input);
+    //     // $param = ['input' => $request->input, 'item' => $item];
+    //     // return view('person.find', $param);
+    // }
+
+    //スコープ nameで検索
+    // public function search(Request $request)
+    // {
+    //     $item = Person::nameEqual($request->input)->first();
+    //     $param = ['input' => $request->input, 'item' => $item];
+    //     return view('person.find', $param);
+    // }
+
+    //スコープ ageで検索
     public function search(Request $request)
     {
-        $item = Person::find($request->input);
+        $min = $request->input * 1;
+        $max = $min + 10;
+        $item = Person::ageGreater($min)
+                        ->ageLessThan($max)
+                        ->first();
         $param = ['input' => $request->input, 'item' => $item];
         return view('person.find', $param);
     }
+
+
+
+
 }
---
+++ app/Scopes/ScopePerson.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Scopes;
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Scope as EloquentScope;
+
+class ScopePerson implements Scope{
+    public function apply(Builder $builder, Model $model)
+    {
+        $builder->where('age', '>', 30);
+    }
+
+}
