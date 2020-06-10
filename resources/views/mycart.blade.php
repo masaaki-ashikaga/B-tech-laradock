b7286d33bc36601ac630f9cd80d37458a9aae029
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-06-10 09:19:00 +0900

    styleの追加

--- resources/views/mycart.blade.php
+++ resources/views/mycart.blade.php
@@ -2,21 +2,27 @@
 
 @section('content')
 <div class="container-fluid">
-    <div class="">
-        <div class="mx-auto" style="max-width:1200px">
-        <h1 style="color: #555555; text-align: center; font-size: 1.2em; padding:24px 0px; font-weight:bold;">{{ Auth::user()->name }}さんのカートの中身</h1>
-        <div class="">
-            <p class="text-center">{{ $message }}</p>
-            <div class="d-flex flex-row flex-wrap">
-                @foreach($my_carts as $my_cart)
-                    <div class="mycart_box">
-                        <p>ユーザーID：{{ $my_cart->user_id }}<br></p>
-                        <p>ストックID：{{ $my_cart->stock_id }}<br></p>
-                    </div>
-                @endforeach
-            </div>
-            <a href="/">商品一覧へ</a>
-        </div>
-    </div>
+   <div class="">
+       <div class="mx-auto" style="max-width:1200px">
+           <h1 class="text-center font-weight-bold" style="color:#555555;  font-size:1.2em; padding:24px 0px;">
+           {{ Auth::user()->name }}さんのカートの中身</h1>
+
+           <div class="">
+               <p class="text-center">{{ $message }}</p><br>
+               <div>
+
+                   @foreach($my_carts as $my_cart)
+                       <div class="mycart_box">
+                           <p>ユーザーID：{{$my_cart->user_id}}</p>
+                           <p>ストックID：{{$my_cart->stock_id}}</p>
+                       </div>
+                   @endforeach
+
+               </div>
+
+               <a href="/">商品一覧へ</a>
+           </div>
+       </div>
+   </div>
 </div>
 @endsection
