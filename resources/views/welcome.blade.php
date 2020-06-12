3a0ee53f83f38ae2e46fb1b97a583ae52082b554
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-06-12 20:38:58 +0900

    レイアウト変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laradock
+                    LaraShop
                 </div>
 
                 <div class="links">
